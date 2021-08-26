cecae9b9bcdfe78d2642c9bde5dc89bd0eb110f3
Brian Oxley <boxley@thoughtworks.com>
2021-08-26 15:06:12 +0800

    Externalize versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "klogging-spring-boot-starter"
 
+pluginManagement {
+    val nexusPublishPluginVersion: String by settings
+
+    plugins {
+        id("io.github.gradle-nexus.publish-plugin") version nexusPublishPluginVersion
+    }
+}
