0a543aadd210eab75bc431d0f31fce476514ecdc
Michael Strasser <michael@michaelstrasser.com>
2021-08-31 21:03:10 +1000

    Remove closeAndReleaseSonatypeStagingRepository gradle task from GitHub build

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.klogging"
-version = "0.1.4"
+version = "0.1.5"
 description = "Starter for using Klogging for logging. An alternative to spring-boot-starter-logging"
 
 val kloggingVersion: String by project
